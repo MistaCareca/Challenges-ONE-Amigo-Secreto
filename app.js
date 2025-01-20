489c706ae4ac4fe3d600978a8509d73ac9f2c540
MistaCareca <brennno0cartaxo@gmail.com>
2025-01-20 17:17:33 -0300

    Implementa uma função para sortear da lista de amigos

--- app.js
+++ app.js
@@ -19,11 +19,26 @@ function atualizarLista() {
         const amigo = amigos[i];
         
         const item = document.createElement("li");
-        
+
         // modifiquei o valor do i apenas na exebiçao
         item.textContent = `${i + 1} - ${amigo}`;
 
         listaAmigos.appendChild(item);
     }
     
+}
+
+function sortearAmigo() {
+    let resultado = document.querySelector("#resultado");
+
+    // feito para evitar repetiçao no sorteio
+
+    if(amigos.length < 2){
+        alert("Adicione mais amigos para o sorteio");
+    } else {
+        let escolhido = Math.floor(Math.random() * amigos.length);
+        resultado.innerHTML = `O Seu amigo(a) é o(a) ${(amigos[escolhido]).toUpperCase()}!`;
+        amigos.splice(escolhido, 1);
+        atualizarLista();
+    }
 }
